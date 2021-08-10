af8e80644a0d3534f64b81cc44444a0db01e0a2c
Raymond von Mizener <vmizener@gmail.com>
2021-08-09 18:22:15 -0700

    Updated error handling; now replies to original message instead of directly sending

--- xkcdbot/bot.py
+++ xkcdbot/bot.py
@@ -19,13 +19,24 @@ bot = commands.Bot(command_prefix=COMMAND_PREFIX, description=BOT_DESCRIPTION)
 async def search(ctx, args):
     embed = discord.Embed()
     resp = requests.post(RELEVANT_XKCD_URL, data={"search": args})
+    fail_msg, failed = "", False
     if not resp.ok:
-        log.error("err1")
+        fail_msg = "Failed to receive response from relevant-xkcd"
+        failed = True
     data = resp.json()
     if not data["success"]:
-        log.error("err2")
+        fail_msg = "Relevant-xkcd reported bad result"
+        failed = True
     results = data["results"]
+    if len(results) == 0:
+        fail_msg = "Somehow, there aren't any relevant XKCDs for that"
+        failed = True
+    if failed:
+        log.error(f"{fail_msg}; args: {args}")
+        await ctx.reply(fail_msg)
+        return
     top_result = results[0]
+    log.info(f"[{top_result['title']}]({top_result['url']})")
     embed.title = top_result["title"]
     embed.description = top_result["titletext"]
     embed.timestamp = datetime.datetime.strptime(
@@ -43,7 +54,7 @@ async def search(ctx, args):
         img = requests.get(top_result["image"], stream=True)
         img.raw.decode_content = True
         shutil.copyfileobj(img.raw, fp)
-        await ctx.send(
+        await ctx.reply(
             file=discord.File(fp.name, filename=top_result["image"]),
             embed=embed,
         )
